c7670a772d371263b0085cd12bed194bc42b6f94
yunjiChoe <hanaa1116@gmail.com>
2021-10-06 21:46:02 +0900

    #5

--- src/Test05.java
+++ src/Test05.java
@@ -13,10 +13,23 @@ public class Test05 {
 				{ 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
 				{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
 			};
-
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr[i].length; j++)
-			System.out.println(arr[i][j]);
+		
+		for (int i = 1; i < arr.length-1; i++) {
+			System.out.println();
+			for (int j = 1; j < arr[i].length-1; j++)
+				if (arr[i][j] != 0
+					&& arr[i][j] <= arr[i-1][j]
+					&& arr[i][j] <= arr[i+1][j]
+					&& arr[i][j] <= arr[i][j+1]
+					&& arr[i][j] <= arr[i][j-1]) {
+					
+					arr[i][j] = arr[i][j] + 1;
+					System.out.print(arr[i][j]);
+				} else {
+					arr[i][j] = arr[i][j];
+					System.out.print(arr[i][j]);
+				}
+				
 		}
 
 	}
